47066dc51e0ef7b2ef0adf6aef99789d75509d23
cassio-bressan <cassiobressan@hotmail.com>
2024-05-31 21:13:09 -0300

    Primeira versão do projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projeto5
 Projeto para criar e implementar um dashboard de aplicativo web em um serviço de nuvem.
+O projeto se trata de anúncios de vendas de carros
---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('vehicles_us.csv')  
+st.header('Dashboard')
+
+hist_button = st.button('Criar histograma')  
+if hist_button:  
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+disp_button = st.button('Criar gráfico de dispersão')
+
+if disp_button:  
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
